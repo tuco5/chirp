9bac1b00020ceda66896a5de9b011994c19d1871
Tudor <tudor.comparan@gmail.com>
2023-07-22 19:54:51 -0600

    posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import { SignIn, useUser, SignOutButton } from "@clerk/nextjs";
+import { api } from "@/utils/api";
 
 export default function Home() {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
   return (
     <>
@@ -14,6 +17,11 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="text-lg text-white">
           {user.isSignedIn ? <SignOutButton /> : <SignIn />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
